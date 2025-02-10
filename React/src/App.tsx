12f6bac021e1d1673e92ae3534f322e5818f6b8f
jprygiel <93471935+Lotheru10@users.noreply.github.com>
2025-02-10 12:42:56 +0100

    Add files via upload

---
+++ React/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Koszyk from "./Components/Koszyk/Koszyk";
+import NowyKoszyk from "./Components/Koszyk/NowyKoszyk";
+import Licznik from "./Components/Licznik/Licznik";
+import NowyLicznik from "./Components/Licznik/NowyLicznik";
+import Formularz from "./Components/Formularze/Formularz";
+import Haslo from "./Components/Formularze/Haslo";
+import Logowanie from "./Components/Formularze/Logowanie";
+import Ternary from "./Components/Inne/Ternary";
+import Aktualizacja from "./Components/Inne/Aktualizacja";
+import Studenci from "./Components/Studenci/Studenci";
+import StudentManager from "./Components/Studenci/StudentManager";
+import Licznik2 from "./Components/Efekty/Licznik";
+import Tytul from "./Components/Efekty/Tytul";
+import Odliczanie from "./Components/Efekty/Odliczanie";
+import Licznik3 from "./Components/Efekty/Licznik3";
+function App() {
+  return <div></div>;
+}
+export default App;
